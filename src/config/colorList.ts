f80c41255e3ef0e98750f59685a8b1259bb9b130
haolichong <wb-hlc336023@alibaba-inc.com>
2021-07-15 16:52:30 +0800

    feat: 颜色

--- src/config/colorList.ts
+++ src/config/colorList.ts
@@ -1,50 +1,50 @@
 export default [
   {
-    lineColor: '#ff85c0',
-    backgroundColor: '#ffd6e7',
+    lineColor: '#ffa39e',
+    backgroundColor: '#ffccc7',
   },
   {
-    lineColor: '#85a5ff',
-    backgroundColor: '#d6e4ff',
-  },
-  {
-    lineColor: '#b37feb',
-    backgroundColor: '#efdbff',
-  },
-  {
-    lineColor: '#5cdbd3',
-    backgroundColor: '#b5f5ec',
-  },
-  {
-    lineColor: '#69c0ff',
-    backgroundColor: '#bae7ff',
-  },
-  {
-    lineColor: '#95de64',
-    backgroundColor: '#d9f7be',
-  },
-  {
-    lineColor: '#fff566',
-    backgroundColor: '#ffffb8',
-  },
-  {
-    lineColor: '#d3f261',
-    backgroundColor: '#f4ffb8',
-  },
-  {
-    lineColor: '#ffd666',
-    backgroundColor: '#fff1b8',
-  },
-  {
-    lineColor: '#ff9c6e',
+    lineColor: '#ffbb96',
     backgroundColor: '#ffd8bf',
   },
   {
-    lineColor: '#ffc069',
+    lineColor: '#ffd591',
     backgroundColor: '#ffe7ba',
   },
   {
-    lineColor: '#ff7875',
-    backgroundColor: '#ffccc7',
+    lineColor: '#ffe58f',
+    backgroundColor: '#fff1b8',
+  },
+  {
+    lineColor: '#eaff8f',
+    backgroundColor: '#f4ffb8',
+  },
+  {
+    lineColor: '#b7eb8f',
+    backgroundColor: '#d9f7be',
+  },
+  {
+    lineColor: '#87e8de',
+    backgroundColor: '#b5f5ec',
+  },
+  {
+    lineColor: '#91d5ff',
+    backgroundColor: '#bae7ff',
+  },
+  // {
+  //   lineColor: '#f4ffb8',
+  //   backgroundColor: '#fcffe6',
+  // },
+  {
+    lineColor: '#adc6ff',
+    backgroundColor: '#d6e4ff',
+  },
+  {
+    lineColor: '#d3adf7',
+    backgroundColor: '#efdbff',
+  },
+  {
+    lineColor: '#ffadd2',
+    backgroundColor: '#ffd6e7',
   },
 ];
