88089a09366fd894d34a72dd0861d861546a51c4
haolichong <wb-hlc336023@alibaba-inc.com>
2021-07-15 17:33:54 +0800

    feat: 颜色生成

--- src/config/colorList.ts
+++ src/config/colorList.ts
@@ -1,50 +1,66 @@
-export default [
-  {
-    lineColor: '#ffa39e',
-    backgroundColor: '#ffccc7',
-  },
-  {
-    lineColor: '#ffbb96',
-    backgroundColor: '#ffd8bf',
-  },
-  {
-    lineColor: '#ffd591',
-    backgroundColor: '#ffe7ba',
-  },
-  {
-    lineColor: '#ffe58f',
-    backgroundColor: '#fff1b8',
-  },
-  {
-    lineColor: '#eaff8f',
-    backgroundColor: '#f4ffb8',
-  },
-  {
-    lineColor: '#b7eb8f',
-    backgroundColor: '#d9f7be',
-  },
-  {
-    lineColor: '#87e8de',
-    backgroundColor: '#b5f5ec',
-  },
-  {
-    lineColor: '#91d5ff',
-    backgroundColor: '#bae7ff',
-  },
-  // {
-  //   lineColor: '#f4ffb8',
-  //   backgroundColor: '#fcffe6',
-  // },
-  {
-    lineColor: '#adc6ff',
-    backgroundColor: '#d6e4ff',
-  },
-  {
-    lineColor: '#d3adf7',
-    backgroundColor: '#efdbff',
-  },
-  {
-    lineColor: '#ffadd2',
-    backgroundColor: '#ffd6e7',
-  },
+const colorList = [
+  '#025DF4',
+  '#DB6BCF',
+  '#2498D1',
+  '#BBBDE6',
+  '#4045B2',
+  '#21A97A',
+  '#FF745A',
+  '#007E99',
+  '#FFA8A8',
+  '#2391FF',
+  '#FFC328',
+  '#A0DC2C',
+  '#946DFF',
+  '#626681',
+  '#EB4185',
+  '#CD8150',
+  '#36BCCB',
+  '#327039',
+  '#803488',
+  '#83BC99',
 ];
+
+const toHex = (color: string) => {
+  const values = color
+    .replace(/rgba?\(/, '')
+    .replace(/\)/, '')
+    .replace(/[\s+]/g, '')
+    .split(',');
+  const a = parseFloat(values[3] || '1');
+  const r = Math.floor(a * parseInt(values[0]) + (1 - a) * 255);
+  const g = Math.floor(a * parseInt(values[1]) + (1 - a) * 255);
+  const b = Math.floor(a * parseInt(values[2]) + (1 - a) * 255);
+  return (
+    '#' +
+    ('0' + r.toString(16)).slice(-2) +
+    ('0' + g.toString(16)).slice(-2) +
+    ('0' + b.toString(16)).slice(-2)
+  );
+};
+
+const toRgba = (val: string, opacity?: string | number) => {
+  //HEX十六进制颜色值转换为RGB(A)颜色值
+  if (/^#/g.test(val)) {
+    const a = val.slice(1, 3);
+    const b = val.slice(3, 5);
+    const c = val.slice(5, 7);
+
+    const rgba = `rgb(${parseInt(a, 16)},${parseInt(b, 16)},${parseInt(c, 16)}${
+      opacity || opacity === 0 ? `,${opacity}` : ''
+    })`;
+
+    return rgba;
+  } else {
+    return '无效颜色值';
+  }
+};
+
+const list = colorList.map((el) => {
+  return {
+    lineColor: toHex(toRgba(el, 0.5)),
+    backgroundColor: toHex(toRgba(el, 0.2)),
+  };
+});
+
+export default list;
